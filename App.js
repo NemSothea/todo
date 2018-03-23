6172cc17e374daafc6b26a73ab7e7340cd2a25df
SotheaNem <nemsothe13@gmail.com>
2018-03-23 11:44:22 +0700

    first comit and finish with simple state

--- App.js
+++ App.js
@@ -5,35 +5,77 @@ import {View,Text,Button,TextInput} from 'react-native';
 //create some stuff
 class App extends React.Component {
   state = {
-    words:"here your words"
+    text:"",
+    todo:[]
+  }
+  addTodo = () => {
+    var newToDo = this.state.text;
+    var arr = this.state.todo;
+    arr.push(newToDo);
+    this.setState({todo:arr,text:""});
+  }
+  deleteToDo = (t) => {
+
+    var arr = this.state.todo;
+    var pos = arr.indexOf(t);
+    arr.splice(pos,1)
+    this.setState({todo:arr});
+  }
+  renderToDos = () => {
+    return this.state.todo.map(t=>{
+      return(
+        <Text key={t} 
+        onPress={()=>{this.deleteToDo(t)}}>
+        {t}</Text>
+      )
+    })
   }
   render(){
     return(
+      <View style={styles.wholeStyle}>
       <View style={styles.viewStyle}>
-          <Text>Hello toDo App, Types somethings</Text>
+          <Text style={styles.headerStyle}>Notes</Text>
           <TextInput
             style={styles.inputStyle}
-            onChangeText={(text)=>this.setState({words:text})}
+            onChangeText={(text)=>this.setState({text})}
+            value={this.state.text}
           />
-          <Button title="ToDo"/>
-          <Text>{this.state.words}</Text>
+          <Button title="Add ToDo" color='green' onPress={this.addTodo}/>
+          {this.renderToDos()}
+      </View>
       </View>
     )
   }
 }
 const styles = {
+  wholeStyle:{
+    flex:1,
+    backgroundColor:'#E0F7FA'
+  },
+  headerStyle:{
+    fontSize:30,
+    marginTop:10,
+    color:'green',
+    fontWeight:'bold'
+  },
   viewStyle:{
-    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    margin:10,
+    marginTop:30
   },
   inputStyle: {
     height:30,
     borderColor:'grey',
     borderWidth:1,
-    width:200,
+    marginLeft:10,
+    marginRight:10,
     borderRadius:5,
+    width:300,
     fontSize:16
+  },
+  buttonStyle: {
+    color:'#4DB6AC'
   }
 }
 
